82da10357bad6c462c81309dd5f9dfbf759819c9
dereck971 <dereck.rauzduel@gmail.com>
2026-03-14 07:13:28 -0400

    Delete analyse-immo/app/layout.jsx

--- analyse-immo/app/layout.jsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "Analyse Immobilière | Pré-étude de faisabilité 24h" };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="fr">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
-    </html>
-  );
-}
